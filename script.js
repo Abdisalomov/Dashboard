777379b9f27334de52e43f1bee874b7abeb44a2b
Nodirbek <nabdisalomov21@gmail.com>
2024-05-18 21:59:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let elInput = document.querySelector('#input');
+
+let changeBtn = () => {
+    let elDiv = document.querySelector('#div');
+    let elInput = document.querySelector('#input');
+    elDiv.style.display = "none";
+    elInput.style.width = "585px";
+    elInput.style.transition = "1s";
+    elBtn.style.transition = "1s";
+    elBtn.style.backgroundColor = "green";
+}
